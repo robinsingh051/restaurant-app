4fd0c3f8582f78c64b3c46b3fd32f26e35e0b146
robinsingh051 <robinsingh051s@gmail.com>
2023-09-28 07:29:23 +0530

    added header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import maelsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "../UI/HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Restaurant App</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={maelsImage} alt="Delicious food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
